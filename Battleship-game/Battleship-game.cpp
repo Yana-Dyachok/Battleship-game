90ffd331bd2d06b2b7b911d6484f37be983aec7a
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-06-03 23:45:42 +0300

    Update Battleship-game.cpp

--- Battleship-game/Battleship-game.cpp
+++ Battleship-game/Battleship-game.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 int main()
 {
+	char chose;
 	srand(time(NULL));
 	Battleship I, Enemy;
-	Enemy.placingShips();
-	I.placingShips();
 	cout << "Hi!Welcome to Battleship. Let's play a game" << endl;
+	Enemy.placingShips();
+	cout << "Choose the way ships are arranged: 1- randomly or 2-place yourself" << endl;
+	cin >> chose;
+	switch (chose) {
+	case '1':I.placingShips(); break;
+	case '2':I.placingShipsMyself(); break;
+	default: cout << "That was a bad chose, please try again"<<endl; break;
+	}
 	while (true) {
 		cout << "\tMY SHIP" << endl;
 		I.printMapEnemy();
