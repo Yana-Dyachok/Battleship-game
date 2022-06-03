f9213e25c59f58e0fb0cf4a401c9992934a44827
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-06-03 22:21:35 +0300

    Update Battleship-game.cpp

--- Battleship-game/Battleship-game.cpp
+++ Battleship-game/Battleship-game.cpp
@@ -1,31 +1,26 @@
 ﻿#include <iostream>
-#include "Battleship.h"
+#include "Battelship.h"
 #include <conio.h>
 using namespace std;
 int main()
 {
 	srand(time(NULL));
-	//int sum = 0;
 	Battleship I, Enemy;
-	I.placingShips();
 	Enemy.placingShips();
-	//cout<<I.placingShips();
-	//cout<<I.ship_rotate();
-
+	I.placingShips();
+	cout << "Hi!Welcome to Battleship. Let's play a game" << endl;
 	while (true) {
-		cout << "\tENEMY SHIP" << endl;
-		I.printMap();
-		I.hittingTarget();
-		system("cls");
 		cout << "\tMY SHIP" << endl;
-		Enemy.printMapEnemy();
-		Enemy.hittingTargetEnemy();
+		I.printMapEnemy();
+		I.hittingTargetEnemy();
+		cout << "\tENEMY SHIP" << endl;
+		Enemy.printMap();
+		Enemy.hittingTarget();
+		system("cls");
 		if (I.getSum() == 0)break;
 		if (Enemy.getSum() == 0)break;
 	}
-	if (I.getSum() == 0)cout << "You win!!!!!!!!" << endl;
+	if (I.getSum() == 0)cout << "Congratulations!!You win!!!!!!!!" << endl;
 	if (Enemy.getSum() == 0)cout << "You lost!!!!!!!!" << endl;
-	else {
-		cout << "Game over" << endl;
-	}
+	cout << "Game over" << endl;
 }
