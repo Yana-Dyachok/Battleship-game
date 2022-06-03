76e437ce63b5e8ee9576d6b8e006bd77cb2c3903
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-06-03 22:20:45 +0300

    Update Battleship.cpp

--- Battleship-game/Battleship.cpp
+++ Battleship-game/Battleship.cpp
@@ -42,15 +42,15 @@ void Battleship::hittingTarget() {
 
 void Battleship::hittingTargetEnemy() {
     std::cout << "Enemy step:" << endl;
-    x = rand() % n + 1;
-    y = rand() % n + 1;
-    if (this->map[this->x - 1][this->y - 1] == 'A') {
+    x = rand() % n;
+    y = rand() % n;
+    if (this->map[this->x][this->y] == 'A') {
         cout << "Enemy hit the target" << endl;
-        this->map[this->x - 1][this->y - 1] = '#';
+        this->map[this->x][this->y] = '#';
         sum--;
     }
     else {
-        this->map[this->x - 1][this->y - 1] = '*';
+        this->map[this->x][this->y] = '*';
         cout << "Enemy missed" << endl;
     }
 }
@@ -65,8 +65,6 @@ void Battleship::placingShips() {
     }
 }
 void Battleship::placingShips() {
-    int mapX [20];
-    int mapY[20];
     for (int i = 0; i < 1; i++)
     {
         x = rand() % n;
@@ -76,41 +74,23 @@ void Battleship::placingShips() {
         this->map[x + 2][y] = 'A';
         this->map[x + 3][y] = 'A';
         sum++;//підрахунок кораблів
-        mapX[i] = x;
-        mapY[i] = y;
-        mapY[i] = y - 1;
-        mapY[i] = y - 2;
-        mapY[i] = y - 3;
-
     }
     for (int i = 0; i < 2; i++)
     {
         x = rand() % n;
         y = rand() % n;
-
-        if (mapX[i] != x and mapY[i] != y) {
             this->map[x][y] = 'A';
             this->map[x][y - 1] = 'A';
             this->map[x][y - 2] = 'A';
             sum++;//підрахунок кораблів
-            mapX[i] = x;
-            mapY[i] = y;
-            mapY[i] = y - 1;
-            mapY[i] = y - 2;
-        }
     }
     for (int i = 0; i < 3; i++)
     {
         x = rand() % n;
         y = rand() % n;
-        if (mapX[i] != x and mapY[i] != y) {
             this->map[x][y] = 'A';
             this->map[x + 1][y] = 'A';
             sum++;//підрахунок кораблів
-            mapX[i] = x;
-            mapX[i] = x+1;
-            mapY[i] = y;
-        }
     }
 
    
@@ -118,106 +98,11 @@ void Battleship::placingShips() {
     {
         x = rand() % n;
         y = rand() % n;
-        if (mapX[i] != x and mapY[i] != y) {
             this->map[x][y] = 'A';
-            sum++;//підрахунок кораблі    
-        }
-    }}
-/*int Battleship::placingShips() {
-    bool setting_is_possible = true;
-    int dir = 0;
-    int temp_x;
-    int temp_y;
-    while (sum < n) {
-        do {
-            this->x = rand() % n+1;
-            this->y = rand() % n+1;
-
-            temp_x = this->x;
-            temp_y = this->y;
-
-            dir = rand() % 4;
-
-            setting_is_possible = true;
-            for (int i = 0; i < n; ++i) {
-                if (this->x < 1 || this->y < 1 || this->x > n || this->y > n) {
-                    setting_is_possible = false;
-                    break;
-                }
-                if (this->map[x][y] == 'A' ||
-                    this->map[x][y + 1] == 'A' ||
-                    this->map[x][y - 1] == 'A' ||
-                    this->map[x + 1][y] == 'A' ||
-                    this->map[x + 1][y + 1] == 'A' ||
-                    this->map[x + 1][y - 1] == 'A' ||
-                    this->map[x - 1][y] == 'A' ||
-                    this->map[x][y] == 'A' ||
-                    this->map[x][y] == 'A')
-                {
-                    setting_is_possible = false;
-                    break;
-                }
-                switch (dir) {
-                case 0:
-                    x++;
-                    break;
-                case 1:
-                    y++;
-                    break;
-                case 2:
-                    x--;
-                    break;
-                case 3:
-                    y--;
-                    break;
-                }
-            }
-        } while (setting_is_possible != 1);
-
-        if (setting_is_possible == 1) {
-            this->x = temp_x;
-            this->y = temp_y;
-            for (int i = 0; i < 4; ++i) {
-                this->map[x][y] = 'A';
-                switch (dir) {
-                case 0:
-                    x++;
-                    break;
-                case 1:
-                    y++;
-                    break;
-                case 2:
-                    x--;
-                    break;
-                case 3:
-                    y--;
-                    break;
-
-                }
-            }
-        }
-        sum++;
+            sum++;//підрахунок кораблів    
     }
-    return this->map[n][n];
 }
 
-
-int Battleship::ship_rotate() {
-    int ship_rate = 4;
-    int ship_count = 1;
-    placingShips();
-    --ship_rate;
-    ++ship_count;
-    placingShips();
-    --ship_rate;
-    ++ship_count;
-    placingShips();
-    --ship_rate;
-    ++ship_count;
-    placingShips();
-    return this->map[n][n];
-}
-*/
 
 Battleship::~Battleship() {
     for (int i = 0; i < n; i++)
