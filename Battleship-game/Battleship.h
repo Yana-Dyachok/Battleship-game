9345a76de9027cd8d4127a40357c51f6ad6d5661
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-06-04 15:45:06 +0300

    Update Battleship.h

--- Battleship-game/Battleship.h
+++ Battleship-game/Battleship.h
@@ -2,27 +2,27 @@
 class Battleship
 {
 private:
-	int x;
-	int y;
-	char** map;
-	char** mapE;
-	int n;
-	int sum;
+	int x;//координати х
+	int y;//координати у
+	char** map;//двовимірний масив-гральне поле 
+	char** mapE;//двовимірний масив-приховане гральне поле 
+	int n;//межі грального поля
+	int sum;//змінна -підраховує кількість кораблів
 public:
-	int getX() { return this->x; }
+	int getX() { return this->x; }//геттери
 	int getY() { return this->y; }
 	int getN() { return this->n; }
 	int getSum() { return this->sum; }
-	void setX(int x) { this->x = x; }
+	void setX(int x) { this->x = x; }//сеттери
 	void setY(int y) { this->y = y; }
 	void setN(int n) { this->n = n; }
 	void setSum(int sum) { this->sum = sum; }
-	Battleship();
-	void printMap();
+	Battleship();//конструктор
+	void printMap();//метод 
 	void printMapEnemy();
 	void placingShips();
 	void placingShipsMyself();
 	void hittingTarget();
 	void hittingTargetEnemy();
-	~Battleship();
+	~Battleship();//деструктор
 };
