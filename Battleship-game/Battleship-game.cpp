8ceb412b818d91ff326661ff133e04ba8173b0c8
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-06-04 16:04:07 +0300

    Update Battleship-game.cpp

--- Battleship-game/Battleship-game.cpp
+++ Battleship-game/Battleship-game.cpp
@@ -4,30 +4,30 @@
 using namespace std;
 int main()
 {
-	char chose;
+	char chose;//змінна вибору методу розташування кораблів
 	srand(time(NULL));
-	Battleship I, Enemy;
+	Battleship I, Enemy;//об'єкти класу Battleship, де I-відповідає гравцю, Enemy-противнику-комп'ютеру
 	cout << "Hi!Welcome to Battleship. Let's play a game" << endl;
-	Enemy.placingShips();
+	Enemy.placingShips();//розташування кораблів противника
 	cout << "Choose the way ships are arranged: 1- randomly or 2-place yourself" << endl;
-	cin >> chose;
+	cin >> chose;//вибір методу розташування
 	switch (chose) {
-	case '1':I.placingShips(); break;
-	case '2':I.placingShipsMyself(); break;
+	case '1':I.placingShips(); break;//розташування кораблів гравця рандомно
+	case '2':I.placingShipsMyself(); break;//розташування кораблів гравця самостійно
 	default: cout << "That was a bad chose, please try again"<<endl; break;
 	}
 	while (true) {
 		cout << "\tMY SHIP" << endl;
-		I.printMapEnemy();
-		I.hittingTargetEnemy();
+		I.printMapEnemy();//вивід грального поля гравця
+		I.hittingTargetEnemy();//постріл противника
 		cout << "\tENEMY SHIP" << endl;
-		Enemy.printMap();
-		Enemy.hittingTarget();
-		system("cls");
-		if (I.getSum() == 0)break;
+		Enemy.printMap();//вивід грального поля противника
+		Enemy.hittingTarget();//постріл гравця 
+		system("cls");//очищення екрану косолі 
+		if (I.getSum() == 0)break;//якщо кількість кораблів рівна нулю - гра закінчується
 		if (Enemy.getSum() == 0)break;
 	}
-	if (I.getSum() == 0)cout << "Congratulations!!You win!!!!!!!!" << endl;
-	if (Enemy.getSum() == 0)cout << "You lost!!!!!!!!" << endl;
+	if (I.getSum() == 0)cout << "Congratulations!!You win!!!!!!!!" << endl;//якщо кількість кораблів закінчиться -виграв гравець
+	if (Enemy.getSum() == 0)cout << "You lost!!!!!!!!" << endl;//якщо кількість кораблів закінчиться -виграв противник
 	cout << "Game over" << endl;
 }
